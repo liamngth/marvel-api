9c227e1c492377cd87577988edb441e7b2d12775
liamnguyen <liam.nguyen@contemi.com.vn>
2019-03-20 17:11:03 +0700

    Add style for pagination

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 import Header from './components/header';
 import Loading from './components/loading';
 import Character from './components/character';
-import Paginator from './components/paginator'
+import Paginator from './components/paginator';
 
 class App extends Component {
   state = {
@@ -14,42 +14,41 @@ class App extends Component {
     characters: [],
     page: 0,
     maxPage: 0,
-    limitPerPage: 20,
+    limitPerPage: 10
   };
 
   componentDidMount() {
     this.search({ sortName: this.state.sortName });
   }
 
-  changePage = (page) => {
+  changePage = page => {
     if (page !== this.state.page) {
       this.search({
-        page,
+        page
       });
     }
   };
 
-  nextPages = (maxPage) => {
+  nextPages = maxPage => {
     this.changePage(maxPage + 1);
   };
 
-  previousPages = (minPage) => {
+  previousPages = minPage => {
     if (minPage > 1) {
-      this.changePage(minPage - 1)
+      this.changePage(minPage - 1);
     }
   };
 
   search = (options = {}) => {
     this.setState({ loading: true });
-    const {
-      page,
-      sortName,
-      limit,
-    } = Object.assign({
-      page: 1,
-      sortName: this.state.sortName,
-      limit: this.state.limitPerPage,
-    }, options);
+    const { page, sortName, limit } = Object.assign(
+      {
+        page: 1,
+        sortName: this.state.sortName,
+        limit: this.state.limitPerPage
+      },
+      options
+    );
     const offset = page ? (page - 1) * limit : 0;
 
     const p = new Promise((resolve, reject) => {
@@ -59,11 +58,11 @@ class App extends Component {
             characters,
             maxPage,
             page: characters.length ? page : 0,
-            limitPerPage: limit,
+            limitPerPage: limit
           });
           resolve({ characters, maxPage, page });
         })
-        .catch((error) => reject(error));
+        .catch(error => reject(error));
     });
     p.done(() => this.setState({ loading: false }));
 
@@ -73,10 +72,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header/>
+        <Header />
         <nav className="navbar App-navbar">
           <ul className="nav navbar-nav">
-            <li className="active"><a href="/"><span className="h4">Characters</span></a></li>
+            <li className="active">
+              <a href="/">
+                <span className="h4">Characters</span>
+              </a>
+            </li>
           </ul>
         </nav>
         <div className="Character">
@@ -85,16 +88,21 @@ class App extends Component {
             <div>Description</div>
           </div>
           <div>
-              {!this.state.loading && this.state.characters.map(c => <Character key={c.id} instance={c}/>)}
+            {!this.state.loading &&
+              this.state.characters.map(c => (
+                <Character key={c.id} instance={c} />
+              ))}
           </div>
         </div>
         {this.state.loading && <Loading />}
-        <Paginator ref={paginator => this.paginator = paginator}
-                   page={this.state.page}
-                   maxPage={this.state.maxPage}
-                   onChangePage={this.changePage}
-                   onNext={this.nextPages}
-                   onPrevious={this.previousPages} />
+        <Paginator
+          ref={paginator => (this.paginator = paginator)}
+          page={this.state.page}
+          maxPage={this.state.maxPage}
+          onChangePage={this.changePage}
+          onNext={this.nextPages}
+          onPrevious={this.previousPages}
+        />
       </div>
     );
   }
